bb57d987305645c12d020baef2256cf06e7fd1f6
rbvaz <romi.alfonzo99@fpuna.com.py>
2022-09-16 18:10:20 -0400

    Filtros en presentacion producto

--- src/app/product-presentation/product-presentation.component.ts
+++ src/app/product-presentation/product-presentation.component.ts
@@ -11,30 +11,47 @@ import { ProductPresentationService } from '../service/product-presentation.serv
 })
 export class ProductPresentationComponent implements OnInit {
   products: ProductPresentation[]=[];
-  productS: ProductPresentation=new ProductPresentation();
+  product: ProductPresentation=new ProductPresentation();
   config: ConfigPage=new ConfigPage();
   message: string = "";
   formValue !: FormGroup;
   showAdd!:boolean;
   showUpdate!:boolean;
+
   constructor(private serviceProductPresentation: ProductPresentationService, private forbuilber: FormBuilder) { }
 
   ngOnInit(): void {
     this.formValue=this.forbuilber.group({
       nombre : [''],
-      descripcion : [''],
-      idProducto : ['']
+      idProducto : [''],
+      filterNameInput : null,
+      filterIdInput : null
     });
     this.config.currentPage=1;
     this.getProducts();
   }
+  //funcion para obtener los valores de los filtros
+  getFilter(){
+    return[
+      this.formValue.get('filterIdInput')?.value ?? null,
+      this.formValue.get('filterNameInput')?.value ?? null
+    ];
+  }
   getProducts():void{
     let inicio= this.config.currentPage-1;
     inicio = inicio*this.config.itemsPerPage;
-    this.serviceProductPresentation.getProducts(inicio).subscribe(
+    const filters=this.getFilter();
+    console.log(filters);
+
+    this.serviceProductPresentation.getProducts(filters,this.config.itemsPerPage,inicio).subscribe(
       entity => this.products = entity.lista,
-      error => console.log('no se pudieron conseguir los productos')
+      error => console.log('no se pudieron conseguir los productos',error)
     );
+  }
+  //funcion para paginar
+  filtrar(){
+    this.config.currentPage=1;
+    this.getProducts();
   }
   //boton a mostrar en el form de relleno
   clickAdd(){
@@ -42,8 +59,18 @@ export class ProductPresentationComponent implements OnInit {
     this.showAdd=true;
     this.showUpdate=false;
   }
+  //funcion para filtrar por id o descripcion
+  cleanFilters():void {
+    this.config.currentPage=1;
+    this.formValue.reset();
+    this.getProducts();
+  }
   saveProduct(): void{
-    this.serviceProductPresentation.createProduct(this.productS).subscribe(
+    this.product.codigo=this.products[this.products.length - 1].codigo + 1;
+    this.product.nombre=this.formValue.value.nombre;
+    this.product.idProducto.idProducto=this.formValue.value.idProducto;
+    console.log(this.product);
+    this.serviceProductPresentation.createProduct(this.product).subscribe(
       () => {
         this.message='Agregado exitosamente'
         let ref=document.getElementById('cancel')
@@ -58,5 +85,4 @@ export class ProductPresentationComponent implements OnInit {
     this.config.currentPage=page;
     this.getProducts();
   }
-
 }
--- src/app/service/product-presentation.service.ts
+++ src/app/service/product-presentation.service.ts
@@ -5,29 +5,34 @@ import { DataList } from '../model/dataList';
 import { ProductPresentation } from '../model/productPresentation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductPresentationService {
-  private api: string = "/stock-nutrinatalia/presentacionProducto";
+  private api: string = '/stock-nutrinatalia/presentacionProducto';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
+  getProducts(
+    filter=[null,null],
+    itemsPerPage: number,
+    inicio: number
+  ): Observable<DataList<ProductPresentation>> {
+    const encode = encodeURIComponent(
+      `{"nombre":${filter[1] != null ? `"${filter[1]}"` : null},"idProducto":{"idTipoProducto":{"idTipoProducto":${filter[0]}}}}`
+    );
+    console.log('El query enviado' + encode);
+    // const url = `?like=S&ejemplo=${encode}&inicio=${inicio}&cantidad=${itemsPerPage}&orderBy=idTipoProducto&orderDir=asc`;
+    const url = `?like=S&ejemplo=${encode}&inicio=${inicio}&cantidad=${itemsPerPage}&orderBy=idPresentacionProducto&orderDir=asc`;
 
-  getProducts( inicio:number): Observable<DataList<ProductPresentation>> {
-    const url= `${this.api}?inicio=${inicio}&cantidad=13&orderBy=idPresentacionProducto&orderDir=asc`;
-
-    return this.http.get<DataList<ProductPresentation>>(url);
+    return this.http.get<DataList<ProductPresentation>>(this.api + url);
   }
 
   createProduct(c: ProductPresentation): Observable<ProductPresentation> {
-    return this.http
-      .post<ProductPresentation>(this.api, c)
-      .pipe(
-        tap( // Log the result or error
-          data => console.log('agregado ' + data),
-          error => console.log("error: " + error)
-        )
-      );
+    return this.http.post<ProductPresentation>(this.api, c).pipe(
+      tap(
+        // Log the result or error
+        (data) => console.log('agregado ' + data),
+        (error) => console.log('error: ' + error)
+      )
+    );
   }
-
 }
